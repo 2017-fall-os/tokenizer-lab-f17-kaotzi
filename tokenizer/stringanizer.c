d6e790b0f634b0f0b40caf350ab48341d33a7190
student <student@localhost.localdomain>
2017-10-01 13:42:40 -0600

    update to tokenizer finally goodish

--- tokenizer/stringanizer.c
+++ tokenizer/stringanizer.c
@@ -16,9 +16,12 @@ char * copyString(char *string){
     		index++;
 		if(string[index]==' ')
 		{
+			copy[index]='\0';
 			return copy;//protects from accidental overwritting outside of words scope. 
 		}
+	
     	}
+	copy[length]='\0';
 	return copy;
 }
 
@@ -27,9 +30,9 @@ char * copyString(char *string){
 int printIt(char *input){
  	int index =0;
 	printf("\nyour single word was: ");
-	while(input[index]!=0)
+	while(input[index]!='\0')
 	{
-		printf("%c",input[index]);
+		printf("%c",input[index]);fflush(stdout);
 		index++;
     	}
 	printf("\n");
@@ -74,15 +77,86 @@ int letterCount(char *input){
 	return count;
 }
 
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////  TOKEN PART/////////////////////////////////////////////////////////////////////////
+
+
+
+
+char **tokenize(char * input)
+{
+  int words = wordCount1(input);
+ char **toks = (char **)malloc(sizeof(char*)*(words+1));
+ 
+//time to fill out the array
+int index =0;
+ 	int count=0;
+ 	int wordses=0;
+	
+	while(wordses<words)
+	{
+		//printf("index = %i, count = %i, wordses = %i\n",index,count,wordses);fflush(stdout);
+		if(input[index]==' ')
+			{
+				
+				count=0;
+		}else{
+			if(count==0)
+				{
+					//printf("hi");fflush(stdout);
+					char *tmp= copyString(input+index);
+					printIt(tmp);					
+					toks[wordses]=tmp;
+					wordses++;
+				}		
+			count++;
+		}
+	index++;
+    	}
+	//char *nool ='\0';
+	
+char * end = (char *)malloc(1);
+end[0]='\0';
+toks[words]= end;
+return toks;
+
+}
+
+
+
+//simple print command to get a full string printed to the screen
+int printAll(char **input){
+ 	int index =0;
+	//printf("\nyour table prints this: ");fflush(stdout);
+//printf(input[index]);fflush(stdout);
+printf("\nyour table prints this: ");fflush(stdout);
+	while(*input[index]!='\0')
+	{
+
+	//printf("\ntable.........: ");fflush(stdout);
+		printIt(input[index]);
+		index++;
+    	}
+	printf("this can be reached\n");
+}
+
+
+
 //the main deal
 int main()
 {
-    //char string[] = "123456  5  6   ";
+    char string[] = "    the brown   fox jumps   \0";
     char *activeWord=copyString(string);
-    
-	printIt(string);
-	printIt(activeWord);
-	
+      
+	//printIt(string);
+	//printIt(activeWord);
+	char **names=tokenize(string);
+	printAll(names);
+	//printIt(names[0]);
+	//printIt(names[1]);	
 
     
     //printf("end found");
